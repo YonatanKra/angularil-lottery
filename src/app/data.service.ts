3e852360cd4ff0918a2959c544d140a8eb576560
Yonatan Kra <yonatan.kr@walkme.com>
2019-07-29 18:09:14 +0300

    Setup name and email resolver from the form's spreadsheet

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -13,12 +13,46 @@ export class DataService {
   public get names() {
     return this.connection
       .pipe(map((res: any) => {
+        const columns = {
+          name: null,
+          email: null
+        };
         const data = res.feed.entry;
-        return data.filter((item, i) => {
-            return i > 1 && (i - 1) % 3 === 0
-        }).map(item => {
-          return {name: item.content.$t}
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].content.$t.toLowerCase() === 'name') {
+              columns.name = i;
+            }
+
+            if (data[i].content.$t.toLowerCase() === 'email') {
+              columns.email = i;
+            }
+
+            // do this only for the header row
+            if (data[i].gs$cell.row > 1) {
+              break;
+            }
+        }
+
+        if (columns.name === null || columns.email === null) {
+          throw new Error(('Must have name and email columns'));
+        }
+
+        const namesAndEmails = data.filter((item, i) => {
+          return item.gs$cell.row > 1 &&
+            (Number(item.gs$cell.col) === columns.name + 1 ||
+              Number(item.gs$cell.col) === columns.email + 1);
         });
+
+        const reducedNamesAndEmails = [];
+        for (let i = 0; i < namesAndEmails.length - 1; i += 2) {
+          reducedNamesAndEmails.push({
+            name: namesAndEmails[i].content.$t,
+            email: namesAndEmails[i + 1].content.$t
+          })
+        }
+
+        return reducedNamesAndEmails;
       }));
   }
 
